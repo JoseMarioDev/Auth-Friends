cd60e1ac5ded0b73607aeccb2cebce8b2a0de614
Jose Montero Jr <josemariodev@gmail.com>
2019-07-18 16:20:17 -0500

    setup routes and links

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { Link, Route, Redirect } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
+import Homepage from './components/Homepage';
 
 function App() {
-  return <div className='App'>test</div>;
+  return (
+    <div className='App'>
+      <div>
+        <Link to='/'>Login</Link>
+        <Link to='/Homepage'>Homepage</Link>
+      </div>
+      <Route exact path='/' component={Login} />
+      <Route
+        exact
+        path='/Homepage'
+        render={props => {
+          const token = localStorage.getItem('token');
+
+          if (!token) {
+            return <Redirect to='/' />;
+          }
+          return <Homepage {...props} />;
+        }}
+      />
+    </div>
+  );
 }
 
 export default App;
